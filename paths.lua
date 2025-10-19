442daa3d4f049e1eaa01ae2b26532c2c25a49060
Flammable Bunny <156060069+Flammable-Bunny@users.noreply.github.com>
2025-10-18 17:41:26 -0700

    Refactor bg_path assignment in paths.lua

--- paths.lua
+++ paths.lua
@@ -2,13 +2,11 @@ local toggles = require("toggles")
 
 local home = os.getenv("HOME")
 
+local home = os.getenv("HOME")
 local waywall_config = home .. "/.config/waywall/"
 
 local paths = {
-	bg_path = toggles.toggle_bg_picture
-	and (waywall_config .. "bgpicpathplaceholder")
-	or nil,
-
+	bg_path = toggles.toggle_bg_picture and (home .. "/.config/waywall/images/background.png") or nil,
 	pacem_path   = home .. "pacemanpathplaceholder",
 	nb_path      = home .. "ninjabrainbotpathplaceholder",
 	overlay_path = home .. "measuringoverlaypathplaceholder",
